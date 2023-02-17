a9e34a682e0120da94762392a08f94f2cbfbd0cf
MUHAMMAD RIDOWAN AHAD <muhammadridowanahad@MUHAMMADs-MacBook-Air.local>
2023-02-17 22:37:49 +0600

    Game tutorial

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 "use strict";
 
-//Selecting elements
+// Game Tutorial
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden-box");
+  overlay.classList.remove("hidden-box");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden-box");
+  overlay.classList.add("hidden-box");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden-box")) {
+    closeModal();
+  }
+});
+
+
+
+//Selecting elements for the game
 
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
